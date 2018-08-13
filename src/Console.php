5589e58778618d43febf9ab6c575c1c6739581f4
Benjamin Morel <benjamin.morel@gmail.com>
2018-08-13 17:47:15 +0200

    First iteration with basic, manual tests

---
+++ src/Console.php
@@ -0,0 +1,377 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Brick\Console;
+
+/**
+ * Console tools.
+ */
+class Console
+{
+    /**
+     * The available text modes.
+     */
+    public const MODE_DEFAULT    = 0;
+    public const MODE_BRIGHT     = 1;
+    public const MODE_DIM        = 2;
+    public const MODE_UNDERSCORE = 4;
+    public const MODE_BLINK      = 5;
+    public const MODE_REVERSE    = 7;
+    public const MODE_HIDDEN     = 8;
+
+    /**
+     * The available text/background colors.
+     */
+    public const COLOR_BLACK      = 0;
+    public const COLOR_RED        = 1;
+    public const COLOR_GREEN      = 2;
+    public const COLOR_YELLOW     = 3;
+    public const COLOR_COLOR_BLUE = 4;
+    public const COLOR_MAGENTA    = 5;
+    public const COLOR_CYAN       = 6;
+    public const COLOR_WHITE      = 7;
+
+    /**
+     * @var bool
+     */
+    private $isTerminal = false;
+
+    /**
+     * Console constructor.
+     */
+    public function __construct()
+    {
+        if (PHP_SAPI !== 'cli') {
+            throw new \RuntimeException('Console can only run in CLI environment.');
+        }
+
+        if (extension_loaded('posix')) {
+            $this->isTerminal = posix_isatty(STDOUT);
+        }
+    }
+
+    /**
+     * Returns whether the program is running in a terminal.
+     *
+     * @return bool
+     */
+    public function isTerminal() : bool
+    {
+        return $this->isTerminal;
+    }
+
+    /**
+     * @param string   $text
+     * @param resource $channel
+     *
+     * @return void
+     */
+    public function println(string $text, $channel = STDOUT) : void
+    {
+        fwrite($channel, $text);
+        fwrite($channel, PHP_EOL);
+    }
+
+    /**
+     * Resets all terminal settings to default.
+     *
+     * If run outside a terminal, this method throws an exception.
+     *
+     * @return void
+     */
+    public function reset() : void
+    {
+        $this->checkTerminal(__FUNCTION__);
+        $this->writeSequence('c');
+    }
+
+    /**
+     * Enables line wrapping.
+     *
+     * If run outside a terminal, this method throws an exception.
+     *
+     * Text wraps to next line if longer than the length of the display area.
+     *
+     * @return void
+     */
+    public function enableLineWrap() : void
+    {
+        $this->checkTerminal(__FUNCTION__);
+        $this->writeSequence('[7h');
+    }
+
+    /**
+     * Disables line wrapping.
+     *
+     * If run outside a terminal, this method throws an exception.
+     *
+     * @return void
+     */
+    public function disableLineWrap() : void
+    {
+        $this->checkTerminal(__FUNCTION__);
+        $this->writeSequence('[7l');
+    }
+
+    /**
+     * Sets the cursor to the home position, at the upper left of the screen.
+     *
+     * If run outside a terminal, this method throws an exception.
+     *
+     * @return void
+     */
+    public function setCursorHome() : void
+    {
+        $this->checkTerminal(__FUNCTION__);
+        $this->writeSequence('[H');
+    }
+
+    /**
+     * Sets the cursor position.
+     *
+     * If run outside a terminal, this method throws an exception.
+     *
+     * @param int $row
+     * @param int $column
+     *
+     * @return void
+     */
+    public function setCursorPosition(int $row, int $column) : void
+    {
+        $this->checkTerminal(__FUNCTION__);
+        $this->writeSequence('[' . $row . ';' . $column . 'H');
+    }
+
+    /**
+     * Moves the cursor up by a number of rows.
+     *
+     * If run outside a terminal, this method throws an exception.
+     *
+     * @param int $rows
+     *
+     * @return void
+     */
+    public function moveCursorUp(int $rows = 1) : void
+    {
+        $this->checkTerminal(__FUNCTION__);
+        $this->writeSequence('[' . $rows . 'A');
+    }
+
+    /**
+     * Moves the cursor down by a number of rows.
+     *
+     * If run outside a terminal, this method throws an exception.
+     *
+     * @param int $rows
+     *
+     * @return void
+     */
+    public function moveCursorDown(int $rows = 1) : void
+    {
+        $this->checkTerminal(__FUNCTION__);
+        $this->writeSequence('[' . $rows . 'B');
+    }
+
+    /**
+     * Moves the cursor forward by a number of columns.
+     *
+     * If run outside a terminal, this method throws an exception.
+     *
+     * @param int $columns
+     *
+     * @return void
+     */
+    public function moveCursorForward(int $columns = 1) : void
+    {
+        $this->checkTerminal(__FUNCTION__);
+        $this->writeSequence('[' . $columns . 'C');
+    }
+
+    /**
+     * Moves the cursor backward by a number of columns.
+     *
+     * If run outside a terminal, this method throws an exception.
+     *
+     * @param int $columns
+     *
+     * @return void
+     */
+    public function moveCursorBackward(int $columns = 1) : void
+    {
+        $this->checkTerminal(__FUNCTION__);
+        $this->writeSequence('[' . $columns . 'D');
+    }
+
+    /**
+     * Saves the current cursor position.
+     *
+     * If run outside a terminal, this method throws an exception.
+     *
+     * @return void
+     */
+    public function saveCursorPosition() : void
+    {
+        $this->checkTerminal(__FUNCTION__);
+        $this->writeSequence('[s');
+    }
+
+    /**
+     * Restores the current cursor position after saveCursorPosition().
+     *
+     * If run outside a terminal, this method throws an exception.
+     *
+     * @return void
+     */
+    public function restoreCursorPosition() : void
+    {
+        $this->checkTerminal(__FUNCTION__);
+        $this->writeSequence('[u');
+    }
+
+    /**
+     * Erases from the current cursor position to the end of the current line.
+     *
+     * If run outside a terminal, this method throws an exception.
+     *
+     * @return void
+     */
+    public function eraseToEndOfLine() : void
+    {
+        $this->checkTerminal(__FUNCTION__);
+        $this->writeSequence('[K');
+    }
+
+    /**
+     * Erases from the current cursor position to the start of the current line.
+     *
+     * If run outside a terminal, this method throws an exception.
+     *
+     * @return void
+     */
+    public function eraseToStartOfLine() : void
+    {
+        $this->checkTerminal(__FUNCTION__);
+        $this->writeSequence('[1K');
+    }
+
+    /**
+     * Erases the entire current line.
+     *
+     * If run outside a terminal, this method throws an exception.
+     *
+     * @return void
+     */
+    public function eraseLine() : void
+    {
+        $this->checkTerminal(__FUNCTION__);
+        $this->writeSequence('[2K');
+    }
+
+    /**
+     * Erases the screen from the current line down to the bottom of the screen.
+     *
+     * If run outside a terminal, this method throws an exception.
+     *
+     * @return void
+     */
+    public function eraseDown() : void
+    {
+        $this->checkTerminal(__FUNCTION__);
+        $this->writeSequence('[J');
+    }
+
+    /**
+     * Erases the screen from the current line up to the top of the screen.
+     *
+     * If run outside a terminal, this method throws an exception.
+     *
+     * @return void
+     */
+    public function eraseUp() : void
+    {
+        $this->checkTerminal(__FUNCTION__);
+        $this->writeSequence('[1J');
+    }
+
+    /**
+     * Erases the screen with the background colour and moves the cursor to home.
+     *
+     * If run outside a terminal, this method throws an exception.
+     *
+     * @return void
+     */
+    public function eraseScreen() : void
+    {
+        $this->checkTerminal(__FUNCTION__);
+        $this->writeSequence('[2J');
+    }
+
+    /**
+     * Sets the text mode.
+     *
+     * If run outside a terminal, this method does nothing.
+     *
+     * @param int $mode A MODE_* constant.
+     *
+     * @return void
+     */
+    public function setTextMode(int $mode) : void
+    {
+        if ($this->isTerminal) {
+            $this->writeSequence('[' . $mode . 'm');
+        }
+    }
+
+    /**
+     * Sets the text color.
+     *
+     * If run outside a terminal, this method does nothing.
+     *
+     * @param int $color A COLOR_* constant.
+     *
+     * @return void
+     */
+    public function setTextColor(int $color) : void
+    {
+        $this->setTextMode(30 + $color);
+    }
+
+    /**
+     * Sets the background color.
+     *
+     * If run outside a terminal, this method does nothing.
+     *
+     * @param int $color A COLOR_* constant.
+     *
+     * @return void
+     */
+    public function setBackgroundColor(int $color) : void
+    {
+        $this->setTextMode(40 + $color);
+    }
+
+    /**
+     * @param string $sequence
+     *
+     * @return void
+     */
+    private function writeSequence(string $sequence) : void
+    {
+        fwrite(STDOUT, "\e" . $sequence);
+    }
+
+    /**
+     * @param string $function
+     *
+     * @return void
+     *
+     * @throws \RuntimeException
+     */
+    private function checkTerminal(string $function) : void
+    {
+        if (! $this->isTerminal) {
+            throw new \RuntimeException($function . '() can only be run in a terminal.');
+        }
+    }
+}
---
+++ tests.php
@@ -0,0 +1,170 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Brick\Console\Console;
+
+$console = new Console();
+
+/* Erase screen */
+
+$console->eraseScreen();
+
+/* Test colors */
+
+$console->setBackgroundColor(Console::COLOR_BLACK);
+
+$console->setTextColor(Console::COLOR_RED);
+$console->println('Red on black');
+
+$console->setTextColor(Console::COLOR_GREEN);
+$console->println('Green on black');
+
+$console->setTextColor(Console::COLOR_YELLOW);
+$console->println('Yellow on black');
+
+$console->setTextColor(Console::COLOR_COLOR_BLUE);
+$console->println('Blue on black');
+
+$console->setTextColor(Console::COLOR_MAGENTA);
+$console->println('Magenta on black');
+
+$console->setTextColor(Console::COLOR_CYAN);
+$console->println('Cyan on black');
+
+$console->setTextColor(Console::COLOR_WHITE);
+$console->println('Green on black');
+
+$console->setTextColor(Console::COLOR_WHITE);
+$console->setBackgroundColor(Console::COLOR_RED);
+$console->println('White on red');
+
+$console->setBackgroundColor(Console::COLOR_GREEN);
+$console->println('White on green');
+
+$console->setBackgroundColor(Console::COLOR_YELLOW);
+$console->println('White on yellow');
+
+$console->setBackgroundColor(Console::COLOR_COLOR_BLUE);
+$console->println('White on blue');
+
+$console->setBackgroundColor(Console::COLOR_MAGENTA);
+$console->println('White on magenta');
+
+$console->setBackgroundColor(Console::COLOR_CYAN);
+$console->println('White on cyan');
+
+$console->setTextColor(Console::COLOR_BLACK);
+$console->setBackgroundColor(Console::COLOR_WHITE);
+$console->println('Black on white');
+
+$console->setTextMode(Console::MODE_DEFAULT);
+$console->println('Back to default');
+
+$console->setTextMode(Console::MODE_BRIGHT);
+$console->println('Bright');
+$console->setTextMode(Console::MODE_DEFAULT);
+
+$console->setTextMode(Console::MODE_DIM);
+$console->println('Dim');
+$console->setTextMode(Console::MODE_DEFAULT);
+
+$console->setTextMode(Console::MODE_UNDERSCORE);
+$console->println('Underscore');
+$console->setTextMode(Console::MODE_DEFAULT);
+
+$console->setTextMode(Console::MODE_BLINK);
+$console->println('Blink');
+$console->setTextMode(Console::MODE_DEFAULT);
+
+$console->setTextMode(Console::MODE_REVERSE);
+$console->println('Reverse');
+$console->setTextMode(Console::MODE_DEFAULT);
+
+$console->setTextMode(Console::MODE_HIDDEN);
+$console->println('Hidden');
+$console->setTextMode(Console::MODE_DEFAULT);
+
+/* Test cursor positioning : draw a red cross */
+
+$draw = function () use ($console) {
+    $console->setTextColor(Console::COLOR_RED);
+    $console->setBackgroundColor(Console::COLOR_RED);
+    echo "X";
+    $console->setTextMode(Console::MODE_DEFAULT);
+    $console->moveCursorBackward();
+};
+
+$console->saveCursorPosition();
+
+$console->moveCursorUp(3);
+$console->moveCursorForward(20);
+$draw();
+
+$console->moveCursorDown(2);
+$draw();
+
+$console->moveCursorBackward(1);
+$console->moveCursorUp(1);
+$draw();
+
+$console->moveCursorForward(2);
+$draw();
+
+$console->moveCursorBackward();
+$draw();
+
+$console->setTextMode(Console::MODE_DEFAULT);
+$console->restoreCursorPosition();
+
+/* Test cursor absolute positioning */
+
+$console->saveCursorPosition();
+
+$console->setCursorHome();
+$console->setBackgroundColor(Console::COLOR_RED);
+$console->setTextColor(Console::COLOR_WHITE);
+echo "HOME";
+$console->setTextMode(Console::MODE_DEFAULT);
+
+$console->setCursorPosition(3, 5);
+$console->setBackgroundColor(Console::COLOR_RED);
+$console->setTextColor(Console::COLOR_WHITE);
+echo "WRITING AT POSITION 3,5";
+$console->setTextMode(Console::MODE_DEFAULT);
+
+$console->restoreCursorPosition();
+
+/* Test erase to end of line */
+
+echo "No 'X' should appear here -> XXXXXXXXXX";
+$console->moveCursorBackward(10);
+$console->eraseToEndOfLine();
+echo "\n";
+
+echo 'XXXXXXXXXX';
+$console->eraseToStartOfLine();
+echo " <- No 'X' should appear here\n";
+
+echo "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget volutpat nunc.";
+$console->moveCursorBackward(51);
+$console->eraseLine();
+echo "<-- No text should appear on either side -->\n";
+
+echo 'Press ENTER to test erase screen up';
+$line = readline('');
+
+$console->saveCursorPosition();
+$console->moveCursorUp(15);
+$console->eraseUp();
+$console->restoreCursorPosition();
+
+echo 'Press ENTER to test erase screen down';
+$line = readline('');
+
+$console->saveCursorPosition();
+$console->moveCursorUp(10);
+$console->eraseDown();
+$console->restoreCursorPosition();
+
+echo "End of tests.\n";
